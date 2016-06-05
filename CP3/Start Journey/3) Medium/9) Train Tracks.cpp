c8b6d7c8c9de7318951a0c2f27c8ff2b01263ae3
Yeferson Gaitan <yefri.gaitan97@gmail.com>
2016-06-05 13:21:51 -0500

    Agregue Medium problems

--- CP3/Start Journey/3) Medium/9) Train Tracks.cpp
+++ CP3/Start Journey/3) Medium/9) Train Tracks.cpp
@@ -1,33 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int get_int() {
-    string s;
-    getline(cin, s);
-    return atoi(s.c_str());
+	string line;
+	getline(cin, line);
+	return atoi(line.c_str());
 }
 
-
-int main () {
-    int N = get_int();
-    while (N--) {
-        int female = 0, male = 0;
-        string s;
-        getline(cin, s);
-        for (int i = 0; i < s.size(); i++) {
-            if ((i+1)%3) {
-                if(s[i] == 'F') {
-                    female++;
-                } else {
-                    male++;
-                }
-            }
-        }
-        if (female == male) {
-            cout<<"LOOP"<<endl;
-        } else {
-            cout<<"NO LOOP"<<endl;
-        }
-    }
-    return 0;
+int main(int argc, char const *argv[])
+{
+	int N = get_int();
+	while (N--) {
+		string line, target = "NO LOOP";
+		getline(cin, line);
+		if (line.size() > 2) {
+			int female = 0, male = 0;
+			for (int i = 0; i < line.size(); i++) {
+				if ((i+1)%3) {
+					if (line[i] == 'F') {
+						female++;
+					} else {
+						male++;
+					}
+				}
+			}
+			if (female == male) target = "LOOP";
+		}
+		cout<<target<<endl;
+	}
+	return 0;
 }

1c610cf10a29dcfd11dc3ee8f019288e050eda79
web <web@web-314461.(none)>
2016-08-22 13:01:44 -0500

    eulerPhi and numDiv

--- CP3/Mathematics/11064 - Number Theory.cpp
+++ CP3/Mathematics/11064 - Number Theory.cpp
@@ -4,28 +4,41 @@ using namespace std;
 
 typedef long long ll;
 
+vector<ll> primes;
+
 ll eulerPhi(ll n) {
-  ll ans = n;
-  for (int i = 2; i * i <= n; i++) {
-    if (n % i == 0) ans -= ans / i;
-    while (n % i == 0) n /= i;
+  ll idx = 0, p = primes[idx], ans = n;
+  while (p * p <= n) {
+    if (n % p == 0) ans -= ans / p;
+    while (n % p == 0) n /= p;
+    p = primes[++idx];
   }
   if (n != 1) ans -= ans / n;
   return ans;
 }
 
 ll numDiv(ll n) {
-  ll ans = 1;
-  for (int i = 2; i * i <= n; i++) {
+  ll idx = 0, p = primes[idx], ans = 1;
+  while (p * p <= n) {
     int power = 0;
-    while (n % i == 0) { n /= i; power++; }
+    while (n % p == 0) { n /= p; power++; }
     ans *= (power + 1);
+    p = primes[++idx];
   }
   if (n != 1) ans *= 2;
   return ans;
 }
 
+void sieve(int upp) {
+  vector<int> v(upp, 1);
+  for (int i = 2; i * i <= upp; i++) if (v[i])
+    for (int j = i * i; j <= upp; j += i) v[j] = 0;
+  for (int i = 2; i <= upp; i++) if (v[i])
+    primes.push_back(i);
+}
+
 int main() {
+  sieve(100000);
   ll n;
   while (cin >> n) {
     ll ans = n - eulerPhi(n) - numDiv(n) + 1;

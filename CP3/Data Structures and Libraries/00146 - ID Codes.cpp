0665d83e7b06077cf78ce317952ab3ff97046f5e
web <web@web-314461.(none)>
2016-08-24 15:56:39 -0500

    DS C++ STL next_permutation and multiset

---
+++ CP3/Data Structures and Libraries/00146 - ID Codes.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	string s;
+	while (cin >> s && s != "#") {
+		if (next_permutation(s.begin(), s.end())) 
+			cout << s << endl;
+		else 
+			cout << "No Successor" << endl;
+	}
+	return 0;
+}

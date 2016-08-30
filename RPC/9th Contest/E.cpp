cbace4f3e7dc6b68ac7d93e3a1189b2e07696244
Yefri97 <yefri.gaitan97@gmail.com>
2016-08-30 10:09:31 -0500

    Created 9th contest RPC 2016

---
+++ RPC/9th Contest/E.cpp
@@ -0,0 +1,36 @@
+/*
+ *  RPC 9-th Contest
+ * 	Problem E: Wheel of Universally Copious Fortune
+ *  Status: Accepted
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	vector<string> vs(n);
+	for (int i = 0; i < n; i++) cin >> vs[i];
+	int q, cs = 0; cin >> q;
+	while (q--) {
+		string s; cin >> s;
+		cout << "Word #" << ++cs << ": " << s << endl;
+		int cont = 0;
+		for (int i = 0; i < n; i++) {
+			string temp = vs[i];
+			if (temp.size() != s.size()) continue;
+			int ok = 1;
+			for (int j = 0; j < temp.size(); j++) {
+				if (s[j] == '-') continue;
+				if (temp[j] != s[j]) ok = 0;
+			}
+			if (ok) {
+				cout << temp << endl;
+				cont++;
+			}
+		}
+		cout << "Total number of candidate words = " << cont << endl;
+	}
+	return 0;
+}

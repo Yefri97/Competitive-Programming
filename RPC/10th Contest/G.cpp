a197a3640de61413bb6ed1d46aa4e43bc24d0fc4
Yefri97 <yefri.gaitan97@gmail.com>
2016-09-12 15:57:08 -0500

    Pinche ejercicio de Hugo :c

---
+++ RPC/10th Contest/G.cpp
@@ -0,0 +1,78 @@
+/*
+ *  RPC 10-th Contest 2016
+ *  Problem G: Generate, Sort and Search
+ *  Status: Accepted
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> ii;
+
+const int MXN = 1e3;
+int a, c, m;
+
+int f(int x) { return (a * x + c) % m; }
+
+ii floydCycleFinding(int x0) {
+  int tortoise = f(x0), hare = f(f(x0));
+  while (tortoise != hare) {
+    tortoise = f(tortoise);
+    hare = f(f(hare));
+  }
+  int mu = 0; hare = x0;
+  while (tortoise != hare) {
+    tortoise = f(tortoise);
+    hare = f(hare);
+    mu++;
+  }
+  int lamba = 1; hare = f(tortoise);
+  while (tortoise != hare) {
+    hare = f(hare);
+    lamba++;
+  }
+  return ii(mu, lamba);
+}
+
+int main() {
+  int cs[MXN + 10]; memset(cs, 0, sizeof cs);
+  int x, q, n;
+  cin >> a >> c >> m >> x >> q >> n;
+  
+  ii u = floydCycleFinding(x);
+  int mu = u.first, lamba = u.second;
+
+  for (int i = 0; i < mu; i++, x = f(x))
+    cs[x]++;
+  
+  int ka = (n - mu) / lamba, kb = (n - mu) % lamba;
+  for (int i = 0; i < lamba; i++, x = f(x))
+    cs[x] += ka + (i < kb);
+
+  for (int i = 0; i < m; i++)
+    cout << cs[i] << " ";
+  cout << endl;
+
+  vector<int> freq(m); freq[0] = cs[0];
+  for (int i = 1; i < m; i++)
+    freq[i] = freq[i - 1] + cs[i];
+
+  for (int i = 0; i < m; i++)
+    cout << freq[i] << " ";
+  cout << endl;
+
+  while (q--) {
+    int nq; cin >> nq;
+    int lo = 0, hi = m - 1;
+    while (lo < hi) {
+      int mid = (lo + hi) / 2;
+      if (freq[mid] < nq)
+        lo = mid + 1;
+      else
+        hi = mid - 1;
+    }
+    if (freq[lo] < nq) lo++;
+    cout << lo << endl;
+  }
+
+  return 0;
+}

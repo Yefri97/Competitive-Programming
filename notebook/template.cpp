5736b73422eb534dee12f1325ca0aadfc6a07534
Yeferson Gaitan <yefri.gaitan97@gmail.com>
2017-05-01 02:43:22 +0000

    Init folder notebook

---
+++ notebook/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define debug(X) cout << #X << " = " << X << endl;
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  /* happy coding :D */
+  return 0;
+}

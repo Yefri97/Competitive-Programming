b5d5801df290331b03364f70c27345addef4caaa
Yeferson Gaitan <yefri.gaitan97@gmail.com>
2017-07-09 21:33:12 +0000

    Updated notebook template

--- notebook/template.cpp
+++ notebook/template.cpp
@@ -3,6 +3,7 @@
 #define debug(X) cout << #X << " = " << X << endl
 #define fori(i,b,e) for (int i = (b); i < (e); ++i)
 #define mod(x,m) ((((x) % (m)) + (m)) % (m))
+#define sq(x) (x) * (x)
 
 using namespace std;
 
@@ -11,7 +12,7 @@ typedef vector<int> vi;
 typedef pair<int, int> ii;
 typedef vector<ii> vii;
 
-const int INF = 1e9;
+const int oo = 1e9;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(0);

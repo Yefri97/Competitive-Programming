80e7d334b2a74a95e282b16a85ad1923faf23c9b
Yeferson Gaitan Gomez <yefri.gaitan97@gmail.com>
2018-05-24 22:05:11 -0500

    Update notebook: fenwick_tree.cpp

--- notebook/data structures/fenwick_tree.cpp
+++ notebook/data structures/fenwick_tree.cpp
@@ -1,21 +1,14 @@
-#define LSOne(S) (S & (-S))
-
-class FenwickTree {
-private:
-	vi ft;
-
-public:
-	FenwickTree() {}
-
-	FenwickTree(int n) { ft.assign(n + 1, 0); }
-
-	int rsq(int b) {
-		int sum = 0; for (; b; b -= LSOne(b)) sum += ft[b];
-		return sum; }
-
-	int rsq(int a, int b) {
-		return rsq(b) - (a == 1 ? 0 : rsq(a - 1)); }
-
-	void adjust(int k, int v) {
-		for (; k < (int)ft.size(); k += LSOne(k)) ft[k] += v; }
+struct FenwickTree {
+	int n;
+	vi data;
+	FenwickTree(int _n) : n(_n), data(vi(n)) {}
+	void update(int at, int by) {
+		while (at < n) data[at] += by, at |= at + 1;
+	}
+	int query(int at) {
+		int res = 0;
+		while (at >= 0) res += data[at], at = (at & (at + 1)) - 1;
+		return res;
+	}
+	int rsq(int a, int b) { return query(b) - query(a - 1); }
 };

931d752a091f94412638c39bf4287284dfdf3e3f
Yeferson Gaitan Gomez <yefri.gaitan97@gmail.com>
2018-06-12 12:17:34 -0500

    Update notebook: template

--- notebook/template.cpp
+++ notebook/template.cpp
@@ -2,16 +2,12 @@
 #define endl '\n'
 #define debug(X) cout << #X << " = " << X << endl
 #define fori(i,b,e) for (int i = (b); i < (e); ++i)
-#define mod(x,m) ((((x) % (m)) + (m)) % (m))
-#define sq(x) (x) * (x)
 
 using namespace std;
 
 typedef long long ll;
 typedef vector<int> vi;
-typedef vector<vi> vvi;
 typedef pair<int, int> ii;
-typedef vector<ii> vii;
 
 const int oo = 1e9;
 
